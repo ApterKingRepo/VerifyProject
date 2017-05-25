2a366b3aad4cc61e9f9fac60b48a1634ca04b05a
ApterKing <wangcccong@outlook.com>
2017-05-25 14:36:20 +0800

    修改Fastfile

--- VerifyProject/fastlane/Jenkinsfile.rb
+++ VerifyProject/fastlane/Jenkinsfile.rb
@@ -1,21 +1,21 @@
 pipeline {
-  agent any   # 你也可以使用 agent {label 'mac_for_iOS'}
+  agent any   // 你也可以使用 agent {label 'mac_for_iOS'}
 
   environment {
-    BASH_PROFILE:'~/.bash_profile'
+    BASH_PROFILE = '~/.bash_profile'
   }
 
   parameters {
       string(name: 'module', defaultValue: 'A,B,C,D,E', description: '组件')
   }
 
-  stage('拉取项目') {
-    steps {
-      git 'https://github.com/ApterKingRepo/VerifyProject.git'
+  stages {
+    stage('拉取项目') {
+      steps {
+        git 'https://github.com/ApterKingRepo/VerifyProject.git'
+      }
     }
-  }
 
-  stages('配置项目') {
     stage('生成配置文件') {
       steps {
         dir('./VerifyProject/Config') {
@@ -39,31 +39,31 @@ pipeline {
         }
       }
     }
-  }
 
-  stage('构建项目') {
-    steps {
-      dir('./VerifyProject/fastlane') {
-        sh 'bundle exec fastlane ios build config:Debug'
+    stage('构建项目') {
+      steps {
+        dir('./VerifyProject/fastlane') {
+          sh 'bundle exec fastlane ios build config:Debug'
+        }
       }
     }
-  }
 
-  stage('部署项目') {
-    steps {
-      dir('./VerifyProject/fastlane') {
-        sh 'bundle exec fastlane ios deploy config:Debug'
+    stage('部署项目') {
+      steps {
+        dir('./VerifyProject/fastlane') {
+          sh 'bundle exec fastlane ios deploy config:Debug'
+        }
       }
     }
   }
 
   post {
     failure {
-      echo '构建失败'
+      echo '构建失败,你可以调用远程通知，或者发送一个Emai'
     }
 
     success {
-      echo '构建成功'
+      echo '构建成功,你可以调用远程通知，或者发送一个Email'
     }
   }
 }
