cde322c4dcb326780736e7d40cacdc52298e6630
ApterKing <wangcccong@outlook.com>
2017-05-26 15:45:33 +0800

    修改ViewController

--- VerifyProject/VerifyProject/ViewController.swift
+++ VerifyProject/VerifyProject/ViewController.swift
@@ -59,6 +59,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        self.view.addSubview(collectionView)
     }
 
     override func didReceiveMemoryWarning() {
